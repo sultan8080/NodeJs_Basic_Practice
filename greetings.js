d40355a3b842d605269fb2df083263ca8bd9680d
Moti-Sultan <motisultannur@gmail.com>
2025-04-23 22:31:31 +0200

    First commit, file created

---
+++ greetings.js
@@ -0,0 +1,4 @@
+const sayHello = (name) => `Bonjour, ${name}, Bienvenu !`;
+const sayBye = (name) => `Goodbye, ${name}. À bientôt !`;
+
+module.exports = { sayHello, sayBye };
---
+++ main.js
@@ -0,0 +1,6 @@
+// main.js
+
+const greetings = require('./greetings');
+
+console.log(greetings.sayHello("Rehan"));
+console.log(greetings.sayBye("Rehan"));
